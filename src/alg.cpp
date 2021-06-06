e9fbfb19b88b394083e6f44f2e75547bb5b1481d
dyoma-bat <78908041+dyoma-bat@users.noreply.github.com>
2021-06-06 23:51:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
+// Данилов Евгений
+// 20-ИСТ-3
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  
+  int d = 0;
+  int a = 0;
+  int b = size - 1;
+  
+  while (a < b) 
+  {
+  int c = (a + b) / 2;
+  if (arr[c] < value)
+  a = c + 1;
+  else
+  b = c;
+  }
+  
+  if (arr[a] == value) 
+  {
+    while (arr[a] == value) 
+    {
+    d++;
+    a++;
+    }
+  }
+  
+  if (d)
+    return d;
+  else
+    return 0;
 }
